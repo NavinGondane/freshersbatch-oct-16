de7f4d0649d1a7032f02e81aa55345a57f6ef5f7
Navinkumar <navinkumar.gondane@capgemini.com>
2024-09-23 18:07:25 +0530

    JS file copied

---
+++ index.js
@@ -0,0 +1,17 @@
+// Declare a variable
+let name = "John";
+
+// Function to greet
+function greet() {
+    console.log("Hello, " + name + "!");
+}
+
+// Call the function
+greet();
+
+// Basic arithmetic operation
+let a = 10;
+let b = 20;
+let sum = a + b;
+
+console.log("The sum of a and b is: " + sum);
